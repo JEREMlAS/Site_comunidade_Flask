95c5261ee4e25cc51c853a1657ad8d22a83567a5
Pedro Jeremias <122833826+JEREMlAS@users.noreply.github.com>
2024-02-18 21:52:40 -0300

    Update __init__.py

--- site_comunidade/__init__.py
+++ site_comunidade/__init__.py
@@ -2,14 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import os
+import sqlalchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5801020b88df23191c9bb0d0b73effb'
-if os.getenv("DATABASE_URL"):
-  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-else:
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -18,7 +15,15 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'cadastro'
 login_manager.login_message_category = 'alert-info'
 
-
+from site_comunidade import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
 
 from site_comunidade import routes
+
+
 
